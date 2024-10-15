be7fc8a40739ca60f7c8492f9e2e13057d2042ff
Yotam Saadon <yotamsaad12@gmail.com>
2024-10-15 23:31:40 +0300

    add slider

--- chimini-landing-page/src/app/components/certifications/certifications.component.ts
+++ chimini-landing-page/src/app/components/certifications/certifications.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-certifications',
   templateUrl: './certifications.component.html',
   styleUrls: ['./certifications.component.scss']
 })
-export class CertificationsComponent {
+export class CertificationsComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const nextButton = document.getElementById('next');
+    const prevButton = document.getElementById('prev');
+    const slider = document.querySelector('.overflow-x-auto');
+
+    if (nextButton && prevButton && slider) {
+      nextButton.addEventListener('click', () => {
+        slider.scrollBy({ left: 200, behavior: 'smooth' });
+      });
+
+      prevButton.addEventListener('click', () => {
+        slider.scrollBy({ left: -200, behavior: 'smooth' });
+      });
+    }
+  }
 }
